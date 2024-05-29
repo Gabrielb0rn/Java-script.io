deb4db7ba8f0b6ef0ab7a81a892bc760b1357c0b
Gabrielb0rn <161833929+Gabrielb0rn@users.noreply.github.com>
2024-05-29 12:33:37 -0300

    Atualização

--- primeira aula 2905/atividades/atividade 2.js
+++ primeira aula 2905/atividades/atividade 2.js
@@ -2,18 +2,18 @@
 // Porém, você possui apenas uma nota de R$100 para pagar.
 // Faça um programa no qual sejam digitados os valores dos itens e mostre na tela valor que você deve receber (troco).
 
-let arroz, batataPalha, sucoDeGarrafa
+//let arroz, batataPalha, sucoDeGarrafa
 
-arroz = Number(10)
-batataPalha = Number(15)
-sucoDeGarrafa = Number(25)
+//arroz = Number(10)
+//batataPalha = Number(15)
+//sucoDeGarrafa = Number(25)
 
-let dinheiro = Number(100)
+//let dinheiro = Number(100)
 
-var escolha = String(prompt('Escolha os itens que deseja comprar:\n1- Arroz\n2- Batata palha\n3-Suco de garrafa'))
+//var escolha = String(prompt('Escolha os itens que deseja comprar:\n1- Arroz\n2- Batata palha\n3-Suco de garrafa'))
 
-switch escolha{
-    case 1:
-    troco = (arroz % dinheiro)
-    alert(troco)
-}
+//switch escolha{
+//    case 1:
+//    troco = (arroz % dinheiro)
+//    alert(troco)
+//}
