e7b0b7a4ee08d2f0ffad95144502f853cb0ce8ce
Gabrielb0rn <161833929+Gabrielb0rn@users.noreply.github.com>
2024-05-29 11:33:18 -0300

    Update primeiraAtividade.js

---
+++ aula 2905/primeiraAtividade.js
@@ -0,0 +1,18 @@
+let nomePessoa = 'Lucas'
+let sobrenomePessoa = "Naspolini"
+//let = constante // string
+var idadePessoa = 28
+var altura = 1.85
+//var = variavel //number
+var usuarioLogado = false
+//booleano
+
+
+// saida de dados
+alert('Olá mundo... cruel! =~')
+alert(idadePessoa)
+alert('O seu sobrenome é ' + sobrenomePessoa)
+//alert = uma funcão que retorna na tela de usuário (print)
+
+
+
