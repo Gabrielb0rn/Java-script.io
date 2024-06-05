0dcced178954b6881efc90fc37e27b6a1fc1d3fa
Gabrielb0rn <gabriel_born@estudante.sesisenai.org.br>
2024-06-05 11:47:18 -0300

    Atualização

--- segunda aula 0506/atividade 5.js
+++ segunda aula 0506/atividade 5.js
@@ -2,5 +2,23 @@
 
 
 
+function calcularIMC(peso, altura) {
+    return peso / (altura * altura);
+}
+
+let peso = Number(prompt('Digite seu peso em kg: '));
+if (isNaN(peso) || peso <= 0) {
+    alert('Peso inválido, tente novamente.');
+} else {
+    let altura = Number(prompt('Digite sua altura em metros: '));
+    if (isNaN(altura) || altura <= 0) {
+        alert('Altura inválida, tente novamente.');
+    } else {
+        let imc = calcularIMC(peso, altura);
+        alert('Seu IMC é: ' + imc.toFixed(2));
+    }
+}
+
+
 
 //x
--- segunda aula 0506/atividade 7.js
+++ segunda aula 0506/atividade 7.js
@@ -4,4 +4,28 @@
 //Caso não tenha havido desperdício e não tenha sobrado chopp, digitar 0 para ambos. Ao final, mostrar a média de litros bebidos por pessoa na festa.
 
 
+
+
+
+
+const convidados = 45;
+const totalChopp = 300;
+
+let desperdicado = Number(prompt('Digite a quantidade de chopp desperdiçado em litros: '));
+if (isNaN(desperdicado) || desperdicado < 0) {
+    alert('Quantidade inválida, tente novamente.');
+} else {
+    let sobrou = Number(prompt('Digite a quantidade de chopp que sobrou em litros: '));
+    if (isNaN(sobrou) || sobrou < 0) {
+        alert('Quantidade inválida, tente novamente.');
+    } else {
+        let consumido = totalChopp - desperdicado - sobrou;
+        let mediaPorPessoa = consumido / convidados;
+        alert('A média de litros bebidos por pessoa na festa é: ' + mediaPorPessoa.toFixed(2) + ' litros.');
+    }
+}
+
+
+
+
 //x
