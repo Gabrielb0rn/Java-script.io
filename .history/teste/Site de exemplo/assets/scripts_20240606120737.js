46b462c7ce89f982a00b38f7764d56f6c1a837f9
Gabrielb0rn <gabriel_born@estudante.sesisenai.org.br>
2024-06-06 12:13:22 -0300

    Teste
    Adicionando um botão misterioso =~

---
+++ .history/teste/Site de exemplo/assets/scripts_20240606120737.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('nav ul li a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+        
+        if (!name || !email || !message) {
+            e.preventDefault();
+            alert('Please fill in all fields.');
+        }
+    });
+});
+
+
+
+const oi = () => {
+ print('Oi :)')
+};
+
+document.getElementById("botão misterioso").addEventListener("click", oi);
