18b7cede27f7023ad18e7f926c3565d124398bd8
Gabrielb0rn <161833929+Gabrielb0rn@users.noreply.github.com>
2024-07-03 11:00:43 -0300

    A

--- 6aula 0307/assets/scripts.js
+++ 6aula 0307/assets/scripts.js
@@ -1,8 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const colorSelect = document.getElementById('colorSelect');
-
-    colorSelect.addEventListener('change', function() {
-        const selectedColor = colorSelect.value;
-        document.body.style.backgroundColor = selectedColor;
+function mudarCor(){
+    document.addEventListener('DOMContentLoaded', function() {
+        const colorSelect = document.getElementById('colorSelect');
+    
+        colorSelect.addEventListener('change', function() {
+            const selectedColor = colorSelect.value;
+            document.body.style.backgroundColor = selectedColor;
+        });
     });
-});
+}
