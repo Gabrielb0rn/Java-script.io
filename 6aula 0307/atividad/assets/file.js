9c1bfc027d3220754c12c90a29ee51b199655845
Gabrielb0rn <161833929+Gabrielb0rn@users.noreply.github.com>
2024-07-03 11:45:54 -0300

    A

---
+++ 6aula 0307/atividad/assets/file.js
@@ -0,0 +1,8 @@
+let elementoSelect = document.getElementById('selectMenu')
+let elementoInputResultado = document.getElementById('Resultado')
+
+function MostraValorSelect(){
+
+    elementoInputResultado.value =  elementoSelect.value
+
+}
